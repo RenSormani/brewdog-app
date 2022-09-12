afa3282c733009ff457841fe98be3fe4ee9150c1
Renato Sormani <ilgiardino@hotmail.co.uk>
2022-09-12 12:02:22 +0100

    Changes to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./Components/container/Navbar/Navbar";
-import Main from "./Components/Home/Home";
 import Random from "./Components/Random/Random";
 import styles from "./App.module.scss";
 import Dashboard from "./Components/container/Dashboard";
+import Home from "./Components/Home/Home";
 
 const App = () => {
   return (
@@ -12,7 +12,7 @@ const App = () => {
       <div className={styles.app}>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Main />} />
+          <Route path="/brewdog-app" element={<Home />} />
           <Route path="allbeers" element={<Dashboard />} />
           <Route path="random" element={<Random />} />
         </Routes>
